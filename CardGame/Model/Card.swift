4d9343d4cd5dd6ce757f318d61f0468b48df046f
chrsfabian17 <chrsfabian17@gmail.com>
2016-10-25 09:27:44 -0600

    Start
    Creted Model Folder and Started Project

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Fabian, Christopher on 10/25/16.
+//  Copyright © 2016 CTECH SWIFT. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Fabian, Christopher on 10/25/16.
+//  Copyright © 2016 CTECH SWIFT. All rights reserved.
+//
+
+import Foundation
