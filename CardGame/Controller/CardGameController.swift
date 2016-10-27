ec69f3b96731bcf4d615823f1cfe1735366a1d86
chrsfabian17 <chrsfabian17@gmail.com>
2016-10-27 09:24:57 -0600

    Updated Storyboard connections. Added files.
    Linked view to CardGameController removed default ViewController.
    Organized the groups of files in the Project navigator.

---
+++ CardGame/Controller/CardGameController.swift
@@ -0,0 +1,14 @@
+//
+//  CardGameController.swift
+//  CardGame
+//
+//  Created by Fabian, Christopher on 10/27/16.
+//  Copyright © 2016 CTECH SWIFT. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    
+}
